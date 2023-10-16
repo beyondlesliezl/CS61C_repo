c7a2c5c29fcbbd4c406f94aec26337e18d77f807
sisiphus <beyondlesliezl@icloud.com>
2023-10-16 21:26:17 +0800

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,16 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *fast = head;
+    node *slow = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            return 1; // 有环
+        }
+    }
+
     return 0;
+
 }
